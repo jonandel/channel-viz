a99fa42af08805975b3b4faa78c143aabb0a1470
jonandel <jon@thebartletts.org>
2013-07-27 11:32:47 +0100

    SCled graph on Y axis to expected limits for hot water

--- follows.js
+++ follows.js
@@ -15,7 +15,7 @@
 	var defaultKey		= 'ShVOcTP17U1E3hOHObSFZu6bSYmK5KkoGnI0hLyrFiOOgDc3', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [739454261], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'Hot Water', // Replaces Xively logo in the header
-		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataDuration	= '6Hours', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
@@ -149,8 +149,10 @@
 										width: 600,
 										height: 200,
 										renderer: 'line',
-										min: parseFloat(datastream.min_value) - .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
-										max: parseFloat(datastream.max_value) + .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
+										//min: parseFloat(datastream.min_value) - .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
+										//max: parseFloat(datastream.max_value) + .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
+										max: 50,
+										min: 10,
 										padding: {
 											top: 0.02,
 											right: 0.02,

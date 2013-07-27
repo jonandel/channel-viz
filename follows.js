a018188032d2512b41fc53315640f5138cff6488
jonandel <jon@thebartletts.org>
2013-07-27 11:38:51 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -152,7 +152,7 @@
 										//min: parseFloat(datastream.min_value) - .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
 										//max: parseFloat(datastream.max_value) + .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
 										max: 50,
-										min: 10,
+										min: 15,
 										padding: {
 											top: 0.02,
 											right: 0.02,
